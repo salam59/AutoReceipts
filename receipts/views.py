4a57e21105d9db8274f0bf054b08370039329d9b
salahuddinfa <salahuddin@forensicalpha.co>
2025-06-20 12:08:36 -0600

    ADD: support for images too

--- receipts/views.py
+++ receipts/views.py
@@ -8,13 +8,17 @@ import os
 from receipts.models import ReceiptMetaData, Receipt, LineItem
 from receipts.serializers import ReceiptMetaDataSerializer, ReceiptDataSerializer, LineItemSerializer
 
+ACCEPTED_FORMATS = ['.png', '.pdf', '.jpg', '.jpeg']
+
 class UploadReceiptView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
     def post(self, request, *args, **kwargs):
         file_obj = request.FILES.get('file')
-        if not file_obj or not file_obj.name.lower().endswith('.pdf'):
-            return Response({'error': 'Only PDF files are allowed.'}, status=status.HTTP_400_BAD_REQUEST)
+        if not file_obj:
+            return Response({'error': 'Attach PDF or Image please'})
+        if not file_obj.name.lower().endswith(tuple(ACCEPTED_FORMATS)):
+            return Response({'error': f'Not a Valid format - Supported Formats: {str(ACCEPTED_FORMATS)}'}, status=status.HTTP_400_BAD_REQUEST)
         receipt_meta = ReceiptMetaData.objects.create(file_name=file_obj.name)
         # Save file to disk in 'uploads/' directory
         os.makedirs('uploads', exist_ok=True)
