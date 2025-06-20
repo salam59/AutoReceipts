7108e24464e37c66c8895c758eaa6d5c73bf6b7a
salahuddinfa <salahuddin@forensicalpha.co>
2025-06-20 12:48:33 -0600

    ADD: validate the uploaded file based on whether it's a receipt or not

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,7 +8,7 @@ import os
 
 from receipts.models import ReceiptMetaData, Receipt, LineItem
 from receipts.serializers import ReceiptMetaDataSerializer, ReceiptDataSerializer, LineItemSerializer
-
+from receipts import utils
 ACCEPTED_FORMATS = ['.png', '.pdf', '.jpg', '.jpeg']
 
 class UploadReceiptView(APIView):
@@ -26,5 +27,22 @@ class UploadReceiptView(APIView):
         with open(file_path, 'wb+') as f:
             for chunk in file_obj.chunks():
                 f.write(chunk)
+        receipt_meta.file_path = file_path
+        receipt_meta.save()
         serializer = ReceiptMetaDataSerializer(receipt_meta)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class ValidateReceiptView(APIView):
+    def get(self, request, receipt_id, *args, **kwargs):
+        receipt_meta = get_object_or_404(ReceiptMetaData, id=receipt_id)
+        file_path, file_id = receipt_meta.file_path, receipt_id
+        receipt_or_not = utils.classify_receipt_or_not(file_path, file_id)
+        if receipt_or_not == 'yes':
+            receipt_meta.is_valid = True
+            receipt_meta.invalid_reason = ''
+        else:
+            receipt_meta.is_valid = False
+            receipt_meta.invalid_reason = 'Not a Receipt'
+        receipt_meta.save()
+        serializer = ReceiptMetaDataSerializer(receipt_meta)
+        return Response(serializer.data)
